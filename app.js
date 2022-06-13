fea1995bae677d093c245c33a0f0c81a7b9b1c3b
Mary190183 <103776945+Mary190183@users.noreply.github.com>
2022-06-13 15:40:27 +0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,18 @@
+function slidesPlagin(activeSlide = 0) {
+const slides = document.querySelectorAll('.slide')
+
+slides[2].classList.add('active')
+
+for (const slide of slides) {
+slide.addEventListener('click', () => {
+clearActiveClasses ()
+slide.classList.add('active')  
+})
+}
+function clearActiveClasses (){
+    slides.forEach((slide) => {
+        slide.classList.remove ('active')
+    });
+}
+}
+slidesPlagin()
